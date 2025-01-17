1ec81dc6d143ff7078c2bbce11cb78e9c7f3abe7
sparta.ddubbu <sm.kim@teamsparta.co>
2025-01-17 11:57:04 +0900

    test: cypress user 정보 - fixture 로 분리하기

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -15,12 +15,17 @@ describe('[E2E] 로그인', () => {
   it('회원 정보 입력하기', () => {
     cy.visit(`${baseUrl}${PAGE_URLS.signIn}`);
 
-    const userInfo = { email: 'hello', password: '123456' };
+    // fixtures에서 사용자 데이터를 로드
+    cy.fixture('users').then((users) => {
+      const userInfo = users[0]; // 첫 번째 사용자 데이터 사용
 
-    cy.get('input[type="text"]').type(userInfo.email);
-    cy.get('input[type="password"]').type(userInfo.password);
-    cy.get('button[type="submit"]').click();
+      // 로그인 폼에 입력
+      cy.get('input[type="text"]').type(userInfo.nickname);
+      cy.get('input[type="password"]').type(userInfo.password);
+      cy.get('button[type="submit"]').click();
 
-    cy.contains('로그아웃').should('be.visible');
+      // 로그인 후 "로그아웃" 텍스트가 화면에 있는지 확인
+      cy.contains('로그아웃').should('be.visible');
+    });
   });
 });
